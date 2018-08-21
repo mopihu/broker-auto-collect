8002654624fed6d6681984680337f98406bec34e
m0py <mopihu@gmail.com>
2018-08-21 23:33:14 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = function BrokerAutoClaim(mod) {
+  let listings = [];
+
+  mod.hook('C_TRADE_BROKER_BUY_IT_NOW', 2, event => {
+    console.log(event);
+    listings.push(event.listing);
+    setTimeout(function() {
+      mod.send('C_TRADE_BROKER_BOUGHT_ITEM_LIST', 1, {})
+    }, 1000);
+  })
+
+  mod.hook('S_TRADE_BROKER_BOUGHT_ITEM_LIST', 1, event => {
+    event.purchases.forEach(function(item) {
+      if (listings.includes(item.purchase)) {
+        mod.send('C_TRADE_BROKER_CALC_BOUGHT_ITEM', 1, {
+          unk1: 524289,
+          unk2: 8,
+          listing: item.purchase
+        })
+        listings.splice(item.purchase, 1);
+      }
+    })
+  })
+}
